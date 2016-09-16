0208fb40c7b0e2f5e7a8678e27c9da422c442908
Keith Showalter <kshowalter@fsec.ucf.edu>
2016-09-16 16:33:46 -0400

    Updated lowdb loading.

--- server/db.js
+++ server/db.js
@@ -1,10 +1,8 @@
 module.exports = function(){
   var low = require('lowdb');
-  var storage = require('lowdb/file-sync');
+
   var normalizedPath = require('path').join(__dirname, '../db/');
-
-  var db = low( normalizedPath+'db.json', { storage:storage });
-
+  const db = low(normalizedPath+'db.json');
 
   return db;
 };
